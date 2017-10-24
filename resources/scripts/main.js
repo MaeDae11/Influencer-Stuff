73e6410cb14fe9ea49a4d33515252d5b666a6a73
Katie <katherinelane11@gmail.com>
2017-10-23 21:32:32 -0400

    adds jquery to video to adjust for width and height

---
+++ resources/scripts/main.js
@@ -0,0 +1,63 @@
+//jQuery is required to run this code
+$( document ).ready(function() {
+  
+      scaleVideoContainer();
+  
+      initBannerVideoSize('.featured-video-container .poster img');
+      initBannerVideoSize('.featured-video-container .filter');
+      initBannerVideoSize('.featured-video-container video');
+  
+      $(window).on('resize', function() {
+          scaleVideoContainer();
+          scaleBannerVideoSize('.featured-video-container .poster img');
+          scaleBannerVideoSize('.featured-video-container .filter');
+          scaleBannerVideoSize('.featured-video-container video');
+      });
+  
+  });
+  
+  function scaleVideoContainer() {
+  
+      var height = $(window).height() + 5;
+      var unitHeight = parseInt(height) + 'px';
+      $('.overall-video-container').css('height',unitHeight);
+  
+  }
+  
+  function initBannerVideoSize(element){
+  
+      $(element).each(function(){
+          $(this).data('height', $(this).height());
+          $(this).data('width', $(this).width());
+      });
+  
+      scaleBannerVideoSize(element);
+  
+  }
+  
+  function scaleBannerVideoSize(element){
+  
+      var windowWidth = $(window).width(),
+      windowHeight = $(window).height() + 5,
+      videoWidth,
+      videoHeight;
+  
+      // console.log(windowHeight);
+  
+      $(element).each(function(){
+          var videoAspectRatio = $(this).data('height')/$(this).data('width');
+  
+          $(this).width(windowWidth);
+  
+          if(windowWidth < 1000){
+              videoHeight = windowHeight;
+              videoWidth = videoHeight / videoAspectRatio;
+              $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+  
+              $(this).width(videoWidth).height(videoHeight);
+          }
+  
+          $('.overall-video-container .featured-ideo-container video').addClass('fadeIn animated');
+  
+      });
+  }
